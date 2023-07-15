cb75a251558bb1c229847575403d2808a715cd23
Andreas Chandra <phoenix.ichigo@yahoo.com>
2023-07-15 08:58:34 +0700

    fix codes follow pylint

--- app/pages/2_regression.py
+++ app/pages/2_regression.py
@@ -1,15 +1,15 @@
-import streamlit as st
-import polars as pl
-import yaml
+"""Webpage for regression model."""
 import sys
 from io import StringIO
 
-from pages.tools.utils import basicsidebar, footer
-from pages.tools.assets import set_assets
-from pages.tools.common import upload_data
-from pages.tools.calc import RunRegression
-
+import polars as pl
+import streamlit as st
+import yaml
 from annotated_text import annotated_text
+from pages.tools.assets import set_assets
+from pages.tools.calc import RunRegression
+from pages.tools.common import upload_data
+from pages.tools.utils import footer
 
 with open("config.yaml", "r") as f:
     st.session_state["config"] = yaml.load(f, Loader=yaml.FullLoader)
@@ -45,7 +45,7 @@ if df is not None:
     all_feats = [i for i in all_measures if i != target_opt]
     features_opt = st.multiselect("dependent variable:", all_feats, key="feature_reg")
     with st.sidebar:
-        st.success("Your total observation is {}".format(dshape[0]))
+        st.success(f"Your total observation is {dshape[0]}")
         show = st.checkbox("show the data raw")
         add_c = st.checkbox("with bias", value=True, key="add_constant")
 
--- app/pages/5_page_five.py
+++ app/pages/5_page_five.py
@@ -1,3 +1,4 @@
+"""Web page for page five."""
 import streamlit as stm
 
 stm.title("This is PageFive Geeks.")

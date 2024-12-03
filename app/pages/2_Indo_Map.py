51498bb0bf0ad557f76b0c5263c58026aee665e2
alamhanz <alamhanz@gmail.com>
2024-12-03 09:30:41 +0900

    preparing the chat

---
+++ app/pages/2_Indo_Map.py
@@ -0,0 +1,25 @@
+"""Web page for chat page."""
+
+import logging
+from functools import partial
+
+import streamlit as st
+import yaml
+from pages.tools.assets import set_assets
+from pages.tools.utils import footer, koen_logger, koencounter, koenprep
+from streamlit_extras.stylable_container import stylable_container
+
+koen_logger("indomap")
+logger = logging.getLogger("indomap")
+koenprep("2")
+
+# asset prep and get data
+with open("config.yaml", "r") as f:
+    st.session_state["config"] = yaml.load(f, Loader=yaml.FullLoader)
+set_assets(st.session_state["config"])
+
+# opening
+st.title("COMING SOON ..")
+
+# footer
+footer()

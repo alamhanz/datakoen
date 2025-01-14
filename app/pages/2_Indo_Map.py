ef198816f5d92bac74b3adf2fe4d8600ee489327
alamhanz <alamhanz@gmail.com>
2025-01-14 10:21:34 +0900

    normalize area

--- app/pages/2_Indo_Map.py
+++ app/pages/2_Indo_Map.py
@@ -43,45 +43,57 @@ with input_container1:
         "indomap__binaries",
     )
 
-output_container = st.container()
-input_container2 = st.container()
+tab1, tab2 = st.tabs(["Maps", "Table"])
 
 # if st.session_state["indomap__binaries"] is not None:
 if df_data is not None:
-    with input_container2:
-        col1, col2 = st.columns(2)
-        with col1:
-            choosen_metric_col = st.selectbox(
-                "Select a metrics:",
-                options=df_data.select_dtypes(include=["number"]).columns,
+    with tab1:
+        output_container = st.container()
+        input_container2 = st.container()
+        with input_container2:
+            col1, col2 = st.columns(2)
+            with col1:
+                choosen_metric_col = st.selectbox(
+                    "Select a metrics:",
+                    options=df_data.select_dtypes(include=["number"]).columns,
+                )
+
+            with col2:
+                col_opt = df_data.select_dtypes(include=["object", "category"]).columns
+                choosen_area_col = st.selectbox(
+                    "Select the column area:",
+                    options=col_opt,
+                )
+
+            # identify
+            identifier = lereng.areaname()
+            area_type = identifier.identify_area(df_data, choosen_area_col)
+            logger.info("area type : %s", area_type)
+
+            # Normalize the name
+            df_data = identifier.normalize(df_data, choosen_area_col)
+            df_data["old_" + choosen_area_col] = df_data[choosen_area_col]
+            df_data[choosen_area_col] = df_data["normalize_area"]
+
+            # make the map
+            map_maker = lereng.chrmap(level=area_type)
+            map_maker.insert(
+                df_data,
+                metric_col=choosen_metric_col,
+                area_col=choosen_area_col,
+                path="app/temp_viz",
             )
+            with open("app/temp_viz/lereng_viz.html", "r") as f:
+                logger.info("read the html")
+                html_content = f.read()
 
-        with col2:
-            choosen_area_col = st.selectbox(
-                "Select the column area:",
-                options=df_data.select_dtypes(include=["object", "category"]).columns,
-            )
+        with output_container:
+            with stylable_container(key="map_container", css_styles=map_container_css):
+                st.components.v1.html(html_content, height=400, width=850)
 
-        # identify
-        identifier = lereng.areaname()
-        area_type = identifier.identify_area(df_data, choosen_area_col)
-        logger.info("area type : %s", area_type)
+    with tab2:
+        st.dataframe(df_data)
 
-        # make the map
-        map_maker = lereng.chrmap(level=area_type)
-        map_maker.insert(
-            df_data,
-            metric_col=choosen_metric_col,
-            area_col=choosen_area_col,
-            path="app/temp_viz",
-        )
-        with open("app/temp_viz/lereng_viz.html", "r") as f:
-            logger.info("read the html")
-            html_content = f.read()
-
-    with output_container:
-        with stylable_container(key="map_container", css_styles=map_container_css):
-            st.components.v1.html(html_content, height=400, width=850)
 
 # footer
 footer()

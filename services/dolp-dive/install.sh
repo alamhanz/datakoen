44b78a09d48864323911199ecd319c7f3ec1d594
alamhanz <alamhanz@gmail.com>
2025-02-04 22:13:36 +0900

    update torch

--- services/dolp-dive/install.sh
+++ services/dolp-dive/install.sh
@@ -3,6 +3,7 @@
 echo "apt get and req"
 apt-get update
 apt-get install -y $(grep -o '^[^#][[:alnum:]-]*' "apt_req.txt")
+rm -rf /var/lib/apt/lists/*
 python -m pip install pip==25.0
 pip install pdm==2.22.3
 pdm install
